e57dba381a3c48732892f435873951d34f616623
azisramdhan <alfatihstudi@gmail.com>
2020-10-03 03:11:41 +0700

    Initial Commit

---
+++ RAWG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RAWG
+//
+//  Created by Azis on 03/10/20.
+//  Copyright © 2020 Stay At Home ID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
